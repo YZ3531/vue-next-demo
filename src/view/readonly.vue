ff42d13076f3a8db214a2928f7df06b2ad1c547e
yeyuhang <yeyuhang@appicplay.com>
2020-06-29 17:18:19 +0800

    readonly_demo

--- src/view/readonly.vue
+++ src/view/readonly.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>readonly</div>
+  <div>
+    <button @click="handleClickRef">Click-Ref</button>
+    <button @click="handleClickReadonly">Click-Readonly</button>
+    <p>{{render}}</p>
+  </div>
 </template>
 
 <script>
+import {ref, watchEffect, readonly, watch} from 'vue';
 export default {
+  setup(){
+    const demo = ref(0)
+    const Readonly = readonly(demo)
+    const render = ref(null)
+    const handleClickRef = () => {
+      demo.value++
+    }
+    const handleClickReadonly = () => {
+      Readonly.value++ // waring
+    }
 
+    watch(()=>{
+      render.value = demo.value
+    },()=>{
+      render.value = Readonly.value
+    })
+
+    return {
+      demo,
+      Readonly,
+      handleClickRef,
+      handleClickReadonly,
+      render
+    }
+  }
 }
 </script>
 
