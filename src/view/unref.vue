7ae3081a85cabe752eda3154fd4aeccbed1339cb
yeyuhang <yeyuhang@appicplay.com>
2020-07-02 11:58:36 +0800

    unref_demo

--- src/view/unref.vue
+++ src/view/unref.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>unref</div>
+  <div>
+    {{list}}   <br>  {{REF}}<br>
+    <button @click="handleClick">click</button>
+  </div>
 </template>
 
 <script>
+import { reactive, toRefs, unref, ref } from 'vue'
 export default {
-
+  setup(){
+    const temp = reactive({
+      list:'111'
+    })
+    const REF = ref(0)
+    const handleClick = ()=>{
+      console.log(unref(temp.list));
+      console.log(unref(REF));
+    }
+    return{
+      ...toRefs(temp),
+      handleClick,
+      REF
+    }
+  }
 }
 </script>
 
